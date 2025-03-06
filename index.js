b12d0d1a3b2c613dc3abb736fc2f739016f0bb0e
unknown <evandriridho555@gmail.com>
2025-03-06 21:22:11 +0700

    Middleware

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const taskRouter = require('./TaskRouter')
+const morgan = require('morgan')
 
+
+const logger = morgan(':method :url :status :res[content-length] :res[content-type] - :response-time ms')
 const app = express()
 
 
 app.use(express.json())
+app.use(logger)
 app.use('/api/v1/tasks', taskRouter)
 
 
