6b4aad2d4e64d65edbf252c85af2e3dcaecfc03b
unknown <evandriridho555@gmail.com>
2025-03-06 19:09:49 +0700

    CRUD EXPRESS

--- TaskController.js
+++ TaskController.js
@@ -1,4 +1,5 @@
 const { randomUUID } = require('node:crypto')
+const { STATUS_CODES } = require('node:http')
 
 class TaskController {
     #tasks = [
@@ -19,110 +20,80 @@ class TaskController {
         },
     ]
 
-    #sendJson = (res, code, data) => {
-        const jsonMessage = JSON.stringify(data)
-        res.writeHead(code, { 'Content-Type': 'application/json', 'Content-Length': jsonMessage.length })
-        res.write(jsonMessage)
-        res.end()
-    }
-
-    #ondata = (req, onendCallback) => {
-        const chunks = []
-        req.on('data', (chunk) => {
-            chunks.push(chunk)
-        })
-        req.on('error', (err) => {
-            console.log('error : ', err)
-        })
-        req.on('end', () => {
-            const jsonRaw = Buffer.concat(chunks).toString()
-            const decode = JSON.parse(jsonRaw)
-            onendCallback(decode)
-        })
-    }
-
     getTasks = (_, res) => {
-        this.#sendJson(res, 200, this.#tasks)
+        res.json(this.#tasks).status(200)
     }
 
     createTask = (req, res) => {
-        this.#ondata(req, (data) => {
-            const { title } = data
-            if (title === undefined || title < 3) {
-                this.#sendJson(res, 400, {
-                    status: STATUS_CODES[400],
-                    message: "Title is required and must be grather than 3"
-                })
-                return
-            }
-            const newTask = { id: randomUUID(), title: title, status: "todo" }
-            this.#tasks.push(newTask)
+        const { title } = req.body
+        if (title === undefined || title.length < 3) {
+            res.json({
+                status: STATUS_CODES[400],
+                message: 'Title must be at least 3 characters'
+            }).status(400)
+            return
+        }
+        const newTask = { id: randomUUID(), title: title, status: "todo" }
+        this.#tasks.push(newTask)
 
-            this.#sendJson(res, 201, newTask)
-        })
+        res.json(newTask).status(201)
     }
 
     deleteTask = (req, res) => {
-        this.#ondata(req, (data) => {
-            const { id } = data
-            if (id === undefined) {
-                this.#sendJson(res, 400, {
-                    status: STATUS_CODES[400],
-                    message: "id is required"
-                })
-                return
-            }
+        const { id } = req.body
+        if (id === undefined) {
+            res.json({
+                status: STATUS_CODES[400],
+                message: "id is required"
+            }).status(400)
+            return
+        }
+        const taskIndex = this.#tasks.findIndex((task) => task.id === id)
 
-            const taskIndex = this.#tasks.findIndex((task) => task.id === id)
+        if (taskIndex < 0) {
+            res.json({
+                status: STATUS_CODES[404],
+                message: "Task not found"
+            }).status(404)
+            return
+        }
 
-            if (taskIndex < 0) {
-                this.#sendJson(res, 404, {
-                    status: STATUS_CODES[404],
-                    message: "Task not found"
-                })
-                return
-            }
+        const deletedTask = this.#tasks[taskIndex]
+        this.#tasks.splice(taskIndex, 1)
 
-            const deletedTask = this.#tasks[taskIndex]
-            this.#tasks.splice(taskIndex, 1)
-
-            this.#sendJson(res, 200, deletedTask)
-        })
+        res.json(deletedTask).status(200)
     }
 
     updateTask = (req, res) => {
-        this.#ondata(req, (data) => {
-            const { id, status } = data
-            if (id === undefined) {
-                this.#sendJson(res, 400, {
-                    status: STATUS_CODES[400],
-                    message: "id is required"
-                })
-                return
-            }
+        const { id, status } = req.body
+        if (id === undefined) {
+            res.json({
+                status: STATUS_CODES[400],
+                message: "id is required"
+            }).status(400)
+            return
+        }
 
-            if (status === undefined) {
-                this.#sendJson(res, 400, {
-                    status: STATUS_CODES[400],
-                    message: "status is required"
-                })
-                return
-            }
+        if (status === undefined) {
+            res.json({
+                status: STATUS_CODES[400],
+                message: "Status is required"
+            }).status(400)
+            return
+        }
 
-            const index = this.#tasks.findIndex((task) => task.id === id)
+        const index = this.#tasks.findIndex((task) => task.id === id)
+        if (index < 0) {
+            res.json({
+                status: STATUS_CODES[404],
+                message: "Task not found"
+            }).status(404)
+            return
+        }
 
-            if (index < 0) {
-                this.#sendJson(res, 404, {
-                    status: STATUS_CODES[404],
-                    message: "Task not found"
-                })
-                return
-            }
+        this.#tasks[index].status = status
 
-            this.#tasks[index].status = status
-
-            this.#sendJson(res, 200, this.#tasks[index])
-        })
+        res.json(this.#tasks[index]).status(200)
     }
 }
 
--- index.js
+++ index.js
@@ -1,81 +1,20 @@
-const { createServer, STATUS_CODES } = require('node:http')
+const express = require('express')
 const { TaskController } = require('./TaskController')
 
-const server = createServer()
-
-
+const app = express()
 const taskController = new TaskController()
 
+app.use(express.json())
 
-server.on('request', (req, res) => {
-    const { method, url } = req
-    switch (url) {
-        case '/api/v1/tasks':
-            switch (method) {
-                case 'GET':
-                    taskController.getTasks(req, res)
-                    break
-                case 'POST':
-                    taskController.createTask(req, res)
-                    break
-                case 'DELETE':
-                    taskController.deleteTask(req, res)
-                    break
-                case 'PUT':
-                    taskController.updateTask(req, res)
-                    break
-                default:
-                    res.writeHead(405, { 'Content-Type': 'application/json' })
-                    res.write(STATUS_CODES[405])
-                    res.end()
-                    break
-            }
-            break
-        default:
-            res.writeHead(404, { 'Content-Type': 'application/json' })
-            res.write()
-            res.end()
-            break
-    }
-})
 
-server.on('close', () => {
-    console.log('Server closed')
-    process.exit(0)
-})
 
-server.on('error', (error) => {
-    if (error) {
-        console.log('Server Error', error)
-        process.exit(1)
-    } else {
-        process.exit(0)
-    }
-})
+app.get('/api/v1/tasks', taskController.getTasks)
+app.post('/api/v1/tasks', taskController.createTask)
+app.delete('/api/v1/tasks', taskController.deleteTask)
+app.put('/api/v1/tasks', taskController.updateTask)
 
-server.on('connection', (socket) => {
-    const { remoteAddress, remotePort } = socket
-    console.log(`Connection from ${remoteAddress}:${remotePort}`)
-})
-
-const handleShutdown = (signal) => {
-    console.log('Started Shutdown Server', { signal })
-    server.close((error) => {
-        if (error) {
-            console.error('Error closing server:', error)
-            process.exit(1)
-        } else {
-            console.log('Shutdown Server Complete')
-            process.exit(0)
-        }
-    })
-    console.log('Shutdown Server Complete', { signal })
-}
-
-process.addListener('SIGINT', handleShutdown)
-process.addListener('SIGTERM', handleShutdown)
 
 const PORT = process.env.PORT || 3000
-server.listen(PORT, 'localhost', () => {
-    console.log(`Server running at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
